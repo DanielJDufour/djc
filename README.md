9a929a2972dde47f3944895aa3b59b1bc1c8949f
DanielJDufour <daniel.j.dufour@gmail.com>
2020-06-17 23:43:01 -0400

    prototype

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # djc
 Command Line Interface for Django with Nifty Scripts
+
+# usage
+Install djc in the same environment as your Django project
+## create a new user
+```bash
+djc create-user --project="$PWD/test/mysite" --username="daniel" --email="example@gmail.com" --password="password" --debug="true"
+```
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from os.path import abspath, dirname, join
+
+root_dir = abspath(dirname(__file__))
+
+with open(join(root_dir, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name = 'djc',
+    scripts=['djc'],
+    version = '0.0.0',
+    description = 'Command Line Interface for Django with Nifty Scripts',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'Daniel J. Dufour',
+    author_email = 'daniel.j.dufour@gmail.com',
+    url = 'https://github.com/DanielJDufour/djc',
+    download_url = 'https://github.com/DanielJDufour/djc/tarball/download',
+    keywords = ['cli','django','python'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        "Programming Language :: Python :: 3",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+    ]
+)
