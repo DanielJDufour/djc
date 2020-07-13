aec5ede447ade223909a836753d14140f2abc556
DanielJDufour <daniel.j.dufour@gmail.com>
2020-07-12 21:29:04 -0500

    added run command

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # djc
-Command Line Interface for Django with Nifty Scripts
+:computer: Command Line Interface for Django with Nifty Scripts
 
 # usage
 Install djc in the same environment as your Django project
 ## create a new user
 ```bash
-djc create-user --project="$PWD/test/mysite" --username="daniel" --email="example@gmail.com" --password="password" --debug="true"
+djc create-user --username="daniel" --email="example@gmail.com" --password="password" --debug="true"
 ```
+## run a file within the Django environment
+```bash
+djc run --filepath seed.py
+```
+or
+```bash
+djc run -f seed.py
+```
+
 ## makemigrations
 ```bash
 djc makemigrations
--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 from os.path import abspath, dirname, join
+from re import sub
 
 root_dir = abspath(dirname(__file__))
 
 with open(join(root_dir, "README.md")) as f:
     long_description = f.read()
 
+# remove emojiis
+long_description = sub(":[a-z]+:", "- ", long_description)
+
 setup(
     name = 'djc',
     scripts=['djc'],
-    version = '0.1.0',
+    version = '0.2.0',
     description = 'Command Line Interface for Django with Nifty Scripts',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,5 +27,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         "Operating System :: OS Independent",
-    ]
+    ],
+    install_requires=["shear"]
 )
