e589919898c68f67d9a7640f46ca929d20a6b43d
DanielJDufour <daniel.j.dufour@gmail.com>
2020-07-23 23:08:50 -0500

    fixed bug

--- setup.py
+++ setup.py
@@ -11,22 +11,22 @@ with open(join(root_dir, "README.md")) as f:
 long_description = sub(":[a-z]+:", "- ", long_description)
 
 setup(
-    name = 'djc',
-    scripts=['djc'],
-    version = '0.7.1',
-    description = 'Command Line Interface for Django with Nifty Scripts',
+    name="djc",
+    scripts=["djc"],
+    version="0.7.2",
+    description="Command Line Interface for Django with Nifty Scripts",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author = 'Daniel J. Dufour',
-    author_email = 'daniel.j.dufour@gmail.com',
-    url = 'https://github.com/DanielJDufour/djc',
-    download_url = 'https://github.com/DanielJDufour/djc/tarball/download',
-    keywords = ['cli','django','python'],
-    classifiers = [
-        'Development Status :: 3 - Alpha',
+    author="Daniel J. Dufour",
+    author_email="daniel.j.dufour@gmail.com",
+    url="https://github.com/DanielJDufour/djc",
+    download_url="https://github.com/DanielJDufour/djc/tarball/download",
+    keywords=["cli", "django", "python"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         "Operating System :: OS Independent",
     ],
-    install_requires=["shear"]
+    install_requires=["shear"],
 )
--- test.sh
+++ test.sh
@@ -8,17 +8,37 @@ echo "removing old database"
 
 rm -f $dirpath/test/mysite/db.sqlite3
 
-pipenv run ./djc makemigrations
+testMigrating() {
+    pipenv run ./djc makemigrations
+    pipenv run ./djc migrate
+}
 
-pipenv run ./djc migrate
+testCreatingNewUser() {
+    pipenv run ./djc create-user --username="daniel" --email="example@gmail.com" --password="password" --debug="true"
+}
 
-pipenv run ./djc create-user --username="daniel" --email="example@gmail.com" --password="password" --debug="true"
-pipenv run ./djc create-user --username="daniel" --email="example@gmail.com" --password="password" --debug="true" --safe="true"
+testCreatingUserThatAlreadyExists() {
+    pipenv run ./djc create-user --username="daniel" --email="example@gmail.com" --password="password" --debug="true" --safe="true"
+}
 
-pipenv run ./djc create-superuser --username="admin" --email="admin@example.org" --password="1028763981623" --debug="true"
-pipenv run ./djc create-superuser --username="admin" --email="admin@example.org" --password="1028763981623" --debug="true" --safe="true"
+testCreatingSuperUser() {
+    pipenv run ./djc create-superuser --username="admin" --email="admin@example.org" --password="1028763981623" --debug="true"
+}
 
+testCreatingSuperUserThatAlreadyExists() {
+    pipenv run ./djc create-superuser --username="admin" --email="admin@example.org" --password="1028763981623" --debug="true" --safe="true"
+}
 
-pipenv run ./djc run --filepath test/exc_test.py
+testCreatingUserWithProjectPath() {
+    pipenv run ./djc create-user --username="daniel" --email="example@gmail.com" --password="password" --debug="true" --project="$PWD/test/mysite" --safe="true"
+}
 
-pipenv run ./djc collecstatic
+testRunning() {
+    pipenv run ./djc run --filepath test/exc_test.py
+}
+
+testCollectingStaticFiles() {
+    pipenv run ./djc collecstatic
+}
+
+. ./shunit2
